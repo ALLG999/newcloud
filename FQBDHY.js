3b0da47d040f1cc124a798e3be8ac4452173f34f
托尼老师五毛钱洗剪吹 <23072523+ALLG999@users.noreply.github.com>
2025-05-16 13:02:03 +0800

    Update FQBDHY.js

--- FQBDHY.js
+++ FQBDHY.js
@@ -10,10 +10,10 @@ let obj = JSON.parse(body);
 // 通用 VIP 字段修改逻辑（可能因接口而异）
 if (obj.data) {
   obj.data.is_vip = true;
-  obj.data.vip_type = "svip";
+  obj.data.vip_type = "vip";
   obj.data.vip_expired_at = 4092599349; // 未来时间戳
   obj.data.expire_time = 4092599349;
-  obj.data.name = "超级会员";
+  obj.data.name = "番茄会员";
   obj.data.remain_day = 9999;
   obj.data.is_auto_renewal = true;
 }
