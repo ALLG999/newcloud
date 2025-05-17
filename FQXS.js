fe8104fef70c4871ac4b28d005106f9261e19401
托尼老师五毛钱洗剪吹 <23072523+ALLG999@users.noreply.github.com>
2025-05-17 17:57:42 +0800

    Update FQXS.js

--- FQXS.js
+++ FQXS.js
@@ -34,9 +34,7 @@
 ^https?:\/\/.*\.*toutiao.*\.com\/.*\.jpg$ url reject-200 
 ^https?:\/\/.*\.*toutiao.*\.com\/.*\.jpeg$ url reject-200 
 
-^https?:\/\/.*zijieapi\.com.* url reject if-header .*live.*
-^https?:\/\/.*zijieapi\.com.* url reject if-header .*img.*
-
+^https?:\/\/.*zijieapi\.com.* url script-request-header https://raw.githubusercontent.com/ALLG999/newcloud/refs/heads/master/FQLJ.js
          
 ^https?:\/\/.+\.pangolin-sdk-toutiao\.com\/api\/ad\/union\/sdk\/(get_ads|stats|settings)\/ - reject
 ^https?:\/\/.+\.pglstatp-toutiao\.com\/.+\/toutiao\.mp4 - reject
