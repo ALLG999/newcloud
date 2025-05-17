dbf6d41d832077ebf4239291bf71044546c69b5a
托尼老师五毛钱洗剪吹 <23072523+ALLG999@users.noreply.github.com>
2025-05-17 18:16:43 +0800

    Update FQXS.js

--- FQXS.js
+++ FQXS.js
@@ -14,7 +14,6 @@
 #!name=番茄小说
 #!desc=番茄小说去广告
 [rewrite_local]
-
 # 本地svip显示（并没有这能力做出来，哈哈哈）
 //^https?:\/\/.*\/(user\/vip_info|vip\/user_info).*$ script-response-body https://raw.githubusercontent.com/ALLG999/newcloud/refs/heads/master/FQBDHY.js
 //^https:\/\/api-access\.pangolin-sdk-toutiao\.com\/.* script-response-body https://raw.githubusercontent.com/ALLG999/newcloud/refs/heads/master/FQXSHY.JS
