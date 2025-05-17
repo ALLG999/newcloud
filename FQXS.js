e5646d4a70d49ff0990f6fe5655773a38ad6f144
托尼老师五毛钱洗剪吹 <23072523+ALLG999@users.noreply.github.com>
2025-05-18 07:29:19 +0800

    更新 FQXS.js

--- FQXS.js
+++ FQXS.js
@@ -43,9 +43,10 @@
 ^https?:\/\/.*\.*toutiao.*\.com\/.*\.png$ url reject-200 
 ^https?:\/\/.*\.*toutiao.*\.com\/.*\.jpg$ url reject-200 
 ^https?:\/\/.*\.*toutiao.*\.com\/.*\.jpeg$ url reject-200 
-
+^https?:\/\/.+\.*webcast.*\.douyin.com url reject-200 
 ^https?:\/\/.*\.zijieapi.*\.com.* url script-request-header https://raw.githubusercontent.com/ALLG999/newcloud/refs/heads/master/FQLJ.js
-^https?:\/\/.*\.pangolin-sdk-toutiao.*\.com.* url script-request-header https://raw.githubusercontent.com/ALLG999/newcloud/refs/heads/master/FQLJ.js
+^https?:\/\/.*\.pangolin-sdk-toutiao.com url reject-200 
+//^https?:\/\/.*\.pangolin-sdk-toutiao.*\.com.* url script-request-header https://raw.githubusercontent.com/ALLG999/newcloud/refs/heads/master/FQLJ.js
          
 ^https?:\/\/.+\.pangolin-sdk-toutiao\.com\/api\/ad\/union\/sdk\/(get_ads|stats|settings)\/ - reject
 ^https?:\/\/.+\.pglstatp-toutiao\.com\/.+\/toutiao\.mp4 - reject
